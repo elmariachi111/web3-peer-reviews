243d92ad4c3a94abe20a01546057a598b2926199
jan-o-e <82890641+jan-o-e@users.noreply.github.com>
2023-01-13 13:18:40 +0000

    created contract infrastructure
    preliminary contracts

--- packages/contracts/script/Deploy.s.sol
+++ packages/contracts/script/Deploy.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import { Token } from "../src/Token.sol";
+import { AntsReviewRoles } from "../src/AntsReviewRoles.sol";
 
 contract DeployScript is Script {
   function setUp() public { }
