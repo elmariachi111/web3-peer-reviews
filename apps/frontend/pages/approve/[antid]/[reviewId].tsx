9db992ac81da4653a27d7e1574e0a7521b2352af
Stefan Adolf <stadolf@gmail.com>
2023-01-15 00:00:37 +0100

    adds Approval call
    Signed-off-by: Stefan Adolf <stadolf@gmail.com>

--- apps/frontend/pages/approve/[antid]/[reviewId].tsx
+++ apps/frontend/pages/approve/[antid]/[reviewId].tsx
@@ -14,15 +14,17 @@ import {
   Link,
   Text,
   Textarea,
+  useToast,
 } from "@chakra-ui/react"
 import { ethers } from "ethers"
 import { useRouter } from "next/router"
-import { useEffect, useMemo, useState } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
 import { useApprover } from "../../../hooks/useApprover"
 import { useOrcidMap } from "../../../hooks/useOrcidMap"
 import { useTokenContract } from "../../../hooks/useTokenContract"
 import { SignedCommitment } from "../../../types"
 import NextLink from "next/link"
+import { useAccount } from "wagmi"
 type PeerReview = {
   accepted: boolean
   peer_reviewer: string
@@ -30,10 +32,12 @@ type PeerReview = {
   orcidProof: string
 }
 
-const VerifyPeerReviewer = (props: { peerReview: PeerReview }) => {
-  const { peerReview } = props
+const VerifyPeerReviewer = (props: {
+  peerReview: PeerReview
+  onValidate?: (valid: boolean) => void
+}) => {
+  const { peerReview, onValidate } = props
 
-  const { orcid } = useOrcidMap(peerReview.peer_reviewer)
   const [privateOrcid, setPrivateOrcid] = useState<string>()
 
   const { orchidContract } = useTokenContract()
@@ -109,7 +113,7 @@ const VerifyPeerReviewer = (props: { peerReview: PeerReview }) => {
       privateSig: validatePrivateAddressSig(proof),
       hash: validateHash(proof),
     }
-    console.log(_validation, allTrue(_validation))
+
     setProof({
       proof,
       validation: _validation,
@@ -184,11 +188,14 @@ const VerifyPeerReviewer = (props: { peerReview: PeerReview }) => {
 export default function Approve() {
   const { query } = useRouter()
   const { antid, reviewId } = query
+  const { address } = useAccount()
 
-  const { contract: reviewContract, chainConfig } = useTokenContract()
+  const { contract: reviewContract } = useTokenContract()
 
   const { approver } = useApprover(antid as string)
   const [peerReview, setPeerReview] = useState<PeerReview>()
+
+  const toast = useToast()
 
   useEffect(() => {
     if (!reviewContract) return
@@ -200,6 +207,19 @@ export default function Approve() {
       setPeerReview(_peerReview)
     })()
   }, [antid, reviewContract, reviewId])
+
+  const approveReview = useCallback(
+    async (antid: string, reviewId: string) => {
+      if (!reviewContract) return
+      const result = await reviewContract.acceptAntReview(antid, reviewId, 0)
+      console.log(result)
+      toast({
+        status: "success",
+        title: "peer review approved",
+      })
+    },
+    [reviewContract, toast]
+  )
 
   return (
     <Flex direction="column">
@@ -222,11 +242,25 @@ export default function Approve() {
       {!peerReview ? (
         "connect your wallet"
       ) : (
-        <Flex direction="column" gap={4}>
-          <Text>Submitted by {peerReview.peer_reviewer} </Text>
-          <Link href={peerReview.reviewHash} isExternal>
-            Review Content
-          </Link>{" "}
+        <Flex direction="column" w="full">
+          <Flex direction="row" w="100%" align="center" justify="space-between">
+            <Flex direction="column" gap={4}>
+              <Text>Submitted by {peerReview.peer_reviewer} </Text>
+              <Text>
+                Peer Review:{" "}
+                <Link href={peerReview.reviewHash} isExternal>
+                  {peerReview.reviewHash}
+                </Link>
+              </Text>
+            </Flex>
+            <Button
+              colorScheme="pink"
+              disabled={approver?.toLowerCase() != address?.toLowerCase()}
+              onClick={() => approveReview(antid as string, reviewId as string)}
+            >
+              Approve
+            </Button>
+          </Flex>
           <VerifyPeerReviewer peerReview={peerReview} />
         </Flex>
       )}
